66135946a6cc4606dcdda293d8c35e5c90d16d92
rajeev <rajeevmaurya9794280914@gmail.com>
2020-09-25 23:38:04 +0530

    Updates

---
+++ precache-manifest.b6480023d26b0a1f81f467a10ecfd928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2e6c54fbc1d3975e80993a137118c5",
+    "url": "/ReactFaceDetectionApp/index.html"
+  },
+  {
+    "revision": "1b77c2d80275fd78d149",
+    "url": "/ReactFaceDetectionApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1e12eaa7d57dc1702a38",
+    "url": "/ReactFaceDetectionApp/static/css/main.4985a48c.chunk.css"
+  },
+  {
+    "revision": "1b77c2d80275fd78d149",
+    "url": "/ReactFaceDetectionApp/static/js/2.6ee0277c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFaceDetectionApp/static/js/2.6ee0277c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e12eaa7d57dc1702a38",
+    "url": "/ReactFaceDetectionApp/static/js/main.c5d59960.chunk.js"
+  },
+  {
+    "revision": "fe2095ce800ad3334698",
+    "url": "/ReactFaceDetectionApp/static/js/runtime-main.09cc4bfe.js"
+  }
+]);
